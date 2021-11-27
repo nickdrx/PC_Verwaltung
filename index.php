b633dfe46bf13dbe18b2c04be4e90c1f4ee5ca47
NDERXEN <nick.derxen@daimler.com>
2021-11-27 13:20:56 +0100

    index feature

--- index.php
+++ index.php
@@ -1,26 +1,35 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+
 $host = 'localhost';
 $user = 'root';
 $password = '';
 $db = 'pcverwaltung';
 //Create Connection
 $mysqli = mysqli_connect($host, $user, $password, $db);
-if ( $mysqli )
-{
 
-}
-else
-{
+if ( $mysqli ){}
+else {
     die('keine Verbindung möglich: ' . mysql_error());
 }
 $query = "SELECT * FROM tblPCs";
 $result = mysqli_query($mysqli, $query);
 
-
-$records = array ();
 $data_index = 0;
+$records = array ();
 
+if (isset($_GET['btn_first'])) {
+  $data_index = $_GET['btn_first'];;
+} else if (isset($_GET['btn_prev'])) {
+  $data_index = $_GET['btn_prev'];;
+} else if (isset($_GET['btn_last'])) {
+  $data_index = $_GET['btn_last'];;
+} else if (isset($_GET['btn_next'])) {
+  $data_index = $_GET['btn_next'];;
+} else{
+$data_index = 0;
+}
 
 $count = 0;
 while ($row = mysqli_fetch_assoc($result)) {
@@ -34,7 +43,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     $records[$count] = $arr;
     $count++;
 }
-$current_record = $records[$data_index];
+
 $anzahl = count($records);
 
 function checkDBTest() {
@@ -42,16 +51,9 @@ function checkDBTest() {
   }
 
 // Next / Before Datensatz Button Event
-if (isset($_POST['btn_first'])) {
-  $data_index=0;
-} else if (isset($_POST['btn_prev'])) {
-  $data_index--;
-} else if (isset($_POST['btn_last'])) {
-  $data_index=$anzahl-1;
-} else if (isset($_POST['btn_next'])) {
-  $data_index++;
-}
-
+$current_record = $records[$data_index];
+$index = $data_index;
+echo $data_index;
 ?>
 
 
@@ -64,7 +66,6 @@ if (isset($_POST['btn_first'])) {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </head>
     <body>
-    <?php echo $data_index ?>
         <div class="container">
             <div class="row">
               <div class="col">
@@ -143,16 +144,16 @@ if (isset($_POST['btn_first'])) {
                             </div>
                             <div class="main_verw_div5">
                                 <div class="data_slider">
-                                <form method="post">
-                                    <div class="data_slider_parent">
+                                <form action="index.php" method="get">
+                                    <div class="data_slider_parent"> 
                                     <div class="data_slider_div1"style="padding: 30px;">
-                                        <input type="submit" class="btn btn-dark" style="float:left;">
+                                        <input type="submit" class="btn btn-dark" name="btn_first" style="float:left;" value="<?php echo $data_index=0 ?>">
                                             
-                                              <svg xmlns="http://www.w3.org/2000/svg" name="btn_first" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+                                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5zM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5z"/>
                                               </svg>
                                         </input>
-                                        <input type="submit" class="btn btn-dark" name="btn_prev" style="float:left; margin-left: 10px;">
+                                        <input type="submit" class="btn btn-dark" name="btn_prev" style="float:left; margin-left: 10px;" value="<?php echo $data_index=$index-1; ?>">
                                             
                                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
@@ -167,21 +168,37 @@ if (isset($_POST['btn_first'])) {
                                         echo "0/0";
                                     } else {
                                         $length_records = count($records);
-                                        echo $data_index . "/" . $length_records;
+                                        echo $index . "/" . $length_records-1;
                                     }
                                     ?>
                                     </div>
                                     <div class="data_slider_div3" style="padding: 30px;">
-                                        <input type="submit" class="btn btn-dark" name="btn_next" style="float:right;">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-right" viewBox="0 0 16 16">
-                                                <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5z"/>
-                                              </svg>
-                                        </input>
-                                        <input type="submit" class="btn btn-dark" name="btn_last" style="float:right; margin-right: 10px;">
+                                    <input type="submit" class="btn btn-dark" name="btn_last" style="float:right;" value="<?php echo $data_index=$anzahl-1; ?>">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                                               </svg>
                                         </input>
+                                        <!-- START -->
+                                        <?php 
+                                        $new_length = $length_records-1;
+                                        if ($index == $new_length) { 
+                                          echo "high" . $new_length;
+                                          ?>
+                                          <!-- Hier einsetzen -->
+                                          <input type="submit" class="btn btn-dark" name="btn_next" style="float:right; margin-right: 10px;"  value="<?php echo $data_index=$index+1; ?>" readonly>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-right" viewBox="0 0 16 16">
+                                                    <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5z"/>
+                                                  </svg>
+                                            </input>
+                                        <?php } else { ?>
+                                        <!-- Hier einsetzen -->
+                                        <input type="submit" class="btn btn-dark" name="btn_next" style="float:right; margin-right: 10px;"  value="<?php echo $data_index=$index+1; ?>">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-right" viewBox="0 0 16 16">
+                                                    <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5z"/>
+                                                  </svg>
+                                            </input>
+                                          <?php } ?>
+                                        <!-- END -->
                                     </div>
                                     </div>
                                     </form>
